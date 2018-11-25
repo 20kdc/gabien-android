c4ce51631e0b19dada8a318ada23fb272e7b1dc2
20kdc <asdd2808@gmail.com>
2018-11-25 13:26:07 +0000

    Attempt to get rid of the window title bar

--- src/gabien/MainActivity.java
+++ src/gabien/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
 
 import java.util.LinkedList;
 import java.util.concurrent.locks.ReentrantLock;
@@ -29,6 +30,7 @@ public class MainActivity extends Activity implements Runnable {
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		SurfaceView surfaceview = new SurfaceView(this);
         surfaceview.setOnTouchListener(new View.OnTouchListener() {
             @Override
